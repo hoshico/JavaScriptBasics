8565332796d0347ef28284e6aafe7ffecca3df5d
hoshico <melancholinista.yasu@gmail.com>
2021-04-20 14:34:11 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-'use strict'
+'use strict';
     
-const score = 85;
+const score = 40;
 
-if (score >= 80) {
-  console.log('Great'):
-} else if (score >= 60) {
-  console.log('Good') {
-} else 
-  console.log('OK...');  
-}
+// if (score >= 80) {
+//   console.log('Great'):
+//  } else {
+//   console.log('OK...');  
+//  }
+
+score >= 80 ? console.log('Great!') : console.log('OK...'); 
