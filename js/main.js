1522e4c4ae132588577f8fa4013432eb786cf4ae
hoshico <melancholinista.yasu@gmail.com>
2021-04-20 16:00:39 +0900

    スコープ分け

--- js/main.js
+++ js/main.js
@@ -1,19 +1,6 @@
 'use strict';
     
-const signal = 'green'; 
-
-switch (signal) {
-  case 'red':
-    console.log('Stop!');
-    break;
-  case 'yellow':
-    console.log('Caution!');
-    break;
-  case 'blue':
-  case 'green':
-    console.log('Go');
-    break;
-  default:
-    console.log('Wrong signal');
-    break;
-}
+{
+  console.log(x);
+  const x = 100;
+ }
