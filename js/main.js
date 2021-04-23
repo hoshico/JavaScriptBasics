b887130fea5aae40c94ba0d14a617e11aa9122ea
hoshico <melancholinista.yasu@gmail.com>
2021-04-23 18:32:41 +0900

    分割代入

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 'use strict';
     
 {
-  con st
- }
+  const score = [80, 90, 40, 70];
+
+  // const [a, b, c ,d] = scores;
+  // console.log(a);
+  // console.log(b);
+  // console.log(c);
+  // console.log(d);
+
+  const [a, b, ...scores] = scores;
+  console.log(a);
+  console.log(b);
+  console.log(others);
+}
